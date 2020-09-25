4adeffe6bc4b618aa727a85017f7ba13558ba627
Bernardo Mesko <bemesko@gmail.com>
2020-09-25 13:26:42 -0300

    Sugestões de Queries de Seleção adicionadas

---
+++ Modelos/Queries De Seleção.sql
@@ -0,0 +1,8 @@
+/*
+IDEIAS PARA SELECTS
+
+- Selecionar todos os jogadores de uma turminha
+- Mostrar todas as armas de um jogador de ID específico
+- Mostrar quantos jogadores têm em cada classe
+- Mostrar todas as turminhas criadas no jogo
+*/
