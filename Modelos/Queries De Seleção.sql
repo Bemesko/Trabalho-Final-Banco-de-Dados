1cb448c610d02c899769acdfa9f11517ed019e20
Bernardo Mesko <bemesko@gmail.com>
2020-09-26 12:26:48 -0300

    Update Queries De Seleção.sql

--- Modelos/Queries De Seleção.sql
+++ Modelos/Queries De Seleção.sql
@@ -1,8 +1,26 @@
-/*
-IDEIAS PARA SELECTS
+-- Mostrar todas as coisas de tudo
+SELECT * FROM armas;
+SELECT * FROM jogadores;
+SELECT * FROM classes;
+SELECT * FROM turminhas;
 
-- Selecionar todos os jogadores de uma turminha
-- Mostrar todas as armas de um jogador de ID específico
-- Mostrar quantos jogadores têm em cada classe
-- Mostrar todas as turminhas criadas no jogo
-*/
+-- Mostra todos os jogadores de uma turminha
+SELECT jogadores.jog_nome, turminhas.tur_nome FROM jogadores
+	INNER JOIN turminhas ON jogadores.fk_turminhas_id = turminhas.tur_id;
+
+-- Mostrar todas as armas de um jogador de ID específico
+SELECT arsenal.*, armas.*
+FROM arsenal INNER JOIN armas
+ON arsenal.fk_arma_id = armas.arm_id
+WHERE arsenal.fk_jogador_id = 1;
+
+-- Mostrar quantos jogadores têm em cada classe
+SELECT COUNT(jogadores.jog_ID)
+	FROM jogadores
+	WHERE jogadores.jog_ID = 7;
+
+-- Mostrar o máximo possível de coisas de um jogador
+SELECT jogadores.jog_nome, jogadores.jog_xp, jogadores.jog_chicletes, turminhas.tur_nome, turminha.tur_slogan, classes.cla_nome, classes.cla_tipo_chapeu 
+	FROM jogadores
+	INNER JOIN turminhas ON jogadores.fk_turminhas_ID = turminhas.tur_id
+	INNER JOIN classes ON jogadores.fk_classes_ID = classes.cla_id;
