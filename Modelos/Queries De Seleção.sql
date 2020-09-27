31510bd0097d38f02e2f8395966bb7f1792b79ad
Bernardo Mesko <55760760+Bemesko@users.noreply.github.com>
2020-09-26 22:06:45 -0300

    Fiz funcionar queries de seleção

--- Modelos/Queries De Seleção.sql
+++ Modelos/Queries De Seleção.sql
@@ -1,4 +1,4 @@
--- Mostrar todas as coisas de tudo
+-- mostra tudo de tudo
 SELECT * FROM armas;
 SELECT * FROM jogadores;
 SELECT * FROM classes;
@@ -7,20 +7,21 @@ SELECT * FROM turminhas;
 -- Mostra todos os jogadores de uma turminha
 SELECT jogadores.jog_nome, turminhas.tur_nome FROM jogadores
 	INNER JOIN turminhas ON jogadores.fk_turminhas_id = turminhas.tur_id;
-
+    
 -- Mostrar todas as armas de um jogador de ID específico
 SELECT arsenal.*, armas.*
 FROM arsenal INNER JOIN armas
 ON arsenal.fk_arma_id = armas.arm_id
 WHERE arsenal.fk_jogador_id = 1;
 
--- Mostrar quantos jogadores têm em cada classe
-SELECT COUNT(jogadores.jog_ID)
-	FROM jogadores
-	WHERE jogadores.jog_ID = 7;
-
--- Mostrar o máximo possível de coisas de um jogador
-SELECT jogadores.jog_nome, jogadores.jog_xp, jogadores.jog_chicletes, turminhas.tur_nome, turminha.tur_slogan, classes.cla_nome, classes.cla_tipo_chapeu 
+-- Mostrar quantos jogadores são bardos
+SELECT COUNT(jogadores.jog_ID) from jogadores 
+	inner join classes ON classes.cla_id = jogadores.fk_classe_id
+    WHERE classes.cla_id = 7;
+    
+-- Mostrar algumas informações do jogador
+SELECT jogadores.jog_nome, jogadores.jog_xp, jogadores.jog_chicletes, 
+turminhas.tur_nome, turminhas.tur_slogan, classes.cla_nome, classes.cla_tipo_chapeu 
 	FROM jogadores
 	INNER JOIN turminhas ON jogadores.fk_turminhas_ID = turminhas.tur_id
-	INNER JOIN classes ON jogadores.fk_classes_ID = classes.cla_id;
+	INNER JOIN classes ON jogadores.fk_classe_ID = classes.cla_id;
